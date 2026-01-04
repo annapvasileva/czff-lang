efde5a252f17189bb7c8d8ee960d2c05474ca490
Sveta <sveta6328s@yandex.ru>
2026-01-04 21:01:39 +0300

    add: attribute for polymorphic serialization

--- Compiler/Parser/AST/Nodes/Expressions/ExpressionNode.cs
+++ Compiler/Parser/AST/Nodes/Expressions/ExpressionNode.cs
@@ -1,3 +1,9 @@
+using System.Text.Json.Serialization;
+using Compiler.Parser.AST.Nodes.Statements;
+
 namespace Compiler.Parser.AST.Nodes.Expressions;
 
+[JsonDerivedType(typeof(BinaryExpressionNode), typeDiscriminator: "binary")]
+[JsonDerivedType(typeof(IdentifierExpressionNode), typeDiscriminator: "identifier")]
+[JsonDerivedType(typeof(LiteralExpressionNode), typeDiscriminator: "literal")]
 public abstract class ExpressionNode : AstNode { }
