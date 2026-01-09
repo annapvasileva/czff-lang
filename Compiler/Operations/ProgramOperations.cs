9d3dd0ea90ad05ebc562b337e89a0d05f3c1526c
jpegMushrum <artem070705@yandex.ru>
2026-01-09 18:33:58 +0300

    [feat] Added function call

--- Compiler/Operations/ProgramOperations.cs
+++ Compiler/Operations/ProgramOperations.cs
@@ -53,3 +53,71 @@ public class Print : IOperation
         return "print";
     }
 }
+
+public class Call(int functionIndex) : IOperation
+{
+    public int FunctionIndex { get; set; } = functionIndex;
+    
+    public void Accept(IOperationVisitor operationVisitor)
+    {
+        operationVisitor.Visit(this);
+    }
+
+    public string GetString()
+    {
+        return "call " + FunctionIndex;
+    }
+}
+
+
+public class Mul : IOperation
+{
+    public void Accept(IOperationVisitor operationVisitor)
+    {
+        operationVisitor.Visit(this);
+    }
+
+    public string GetString()
+    {
+        return "mul";
+    }
+}
+
+public class Min : IOperation
+{
+    public void Accept(IOperationVisitor operationVisitor)
+    {
+        operationVisitor.Visit(this);
+    }
+
+    public string GetString()
+    {
+        return "min";
+    }
+}
+
+public class Sub : IOperation
+{
+    public void Accept(IOperationVisitor operationVisitor)
+    {
+        operationVisitor.Visit(this);
+    }
+
+    public string GetString()
+    {
+        return "sub";
+    }
+}
+
+public class Div : IOperation
+{
+    public void Accept(IOperationVisitor operationVisitor)
+    {
+        operationVisitor.Visit(this);
+    }
+
+    public string GetString()
+    {
+        return "div";
+    }
+}
