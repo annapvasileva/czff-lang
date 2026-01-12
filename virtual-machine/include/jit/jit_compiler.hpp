0c0ec83c9963c41d7144d8056e3bfd04902083f7
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-01-12 17:50:05 +0300

    Update jit_compiler.hpp

--- virtual-machine/include/jit/jit_compiler.hpp
+++ virtual-machine/include/jit/jit_compiler.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
+#include <memory>
+#include "common.hpp"
+
 namespace czffvm_jit {
 
+using namespace czffvm;
+
+class CompiledRuntimeFunction {
+    template<typename FuncType>
+    FuncType getFunction() const;
+    
+    virtual ~CompiledRuntimeFunction() = default;
+};
+
 class JitCompiler {
-    // TODO
+    virtual ~JitCompiler() = default;
+    
+    virtual bool CanCompile(czffvm::OperationCode opcode) = 0;
+    virtual bool CanCompile(czffvm::Operation op) = 0;
+    virtual std::unique_ptr<CompiledRuntimeFunction> CompileFunction(
+        const RuntimeFunction& function) = 0;
+    
+    static std::unique_ptr<JitCompiler> create();
 };
 
 }  // namespace czffvm_jit
