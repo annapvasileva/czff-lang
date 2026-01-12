5a6cd3572a2951b769b40fb90f75e34c48cde837
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-01-12 17:50:05 +0300

    Update jit_compiler.hpp

--- virtual-machine/include/jit/jit_compiler.hpp
+++ virtual-machine/include/jit/jit_compiler.hpp
@@ -8,24 +8,13 @@ namespace czffvm_jit {
 using namespace czffvm;
 
 class CompiledRuntimeFunction {
-public:
     template<typename FuncType>
-    FuncType getFunction() const {
-        return reinterpret_cast<FuncType>(GetCode());
-    }
-
+    FuncType getFunction() const;
     
-    ~CompiledRuntimeFunction() = default;
-    virtual void* GetCode() const = 0;
-    virtual size_t GetSize() const = 0;
-    virtual uint16_t GetNameIndex() const = 0;
-    virtual uint16_t GetReturnTypeIndex() const = 0;
-    
-    explicit operator bool() const { return GetCode() != nullptr; }
+    virtual ~CompiledRuntimeFunction() = default;
 };
 
 class JitCompiler {
-public:
     virtual ~JitCompiler() = default;
     
     virtual bool CanCompile(czffvm::OperationCode opcode) = 0;
