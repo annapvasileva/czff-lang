e34bdf84ac9162f67ffc7266474b5f1980722bb0
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-01-12 13:44:31 +0300

    feat CompiledRuntimeFunction

--- virtual-machine/src/jit/jit_x86_64.cpp
+++ virtual-machine/src/jit/jit_x86_64.cpp
@@ -7,6 +7,28 @@
 
 namespace czffvm_jit {
 
+class CompiledRuntimeFunction {
+private:
+    asmjit::x86::Gp stackPtr;
+    asmjit::x86::Gp stackBase;
+    asmjit::x86::Gp framePtr;
+    asmjit::x86::Gp pc;
+    asmjit::x86::Gp memPtr;
+    
+    asmjit::x86::Assembler* a;
+    std::vector<asmjit::v1_21::Label> labels;
+
+public:
+    CompiledRuntimeFunction(
+        asmjit::x86::Gp stackPtr,
+        asmjit::x86::Gp stackBase,
+        asmjit::x86::Gp framePtr,
+        asmjit::x86::Gp pc,
+        asmjit::x86::Gp memPtr,
+        asmjit::x86::Assembler* a,
+        std::vector<asmjit::v1_21::Label> labels
+    ) : stackPtr(stackPtr), stackBase(stackBase), framePtr(framePtr), pc(pc), memPtr(memPtr), a(a), labels(labels) {}
+};
 class X86JitCompiler : public JitCompiler {
     // TODO
 };
