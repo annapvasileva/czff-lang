737d600c5e6675dc661138edd1652a7ee51fff79
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-01-12 17:52:07 +0300

    Update jit_x86_64.cpp

--- virtual-machine/src/jit/jit_x86_64.cpp
+++ virtual-machine/src/jit/jit_x86_64.cpp
@@ -2,56 +2,54 @@
 #include <vector>
 #include <asmjit/x86.h>
 
-#include "jit/jit_compiler.hpp"
+#include "jit/jit_x86_64.hpp"
 #include "common.hpp"
 
 namespace czffvm_jit {
 
 using namespace czffvm;
 
-class CompiledRuntimeFunction {
-private:
-    asmjit::x86::Gp stackPtr;
-    asmjit::x86::Gp stackBase;
-    asmjit::x86::Gp framePtr;
-    asmjit::x86::Gp pc;
-    asmjit::x86::Gp memPtr;
+CompiledRuntimeFunction X86JitCompiler::CompileFunction(RuntimeFunction& function) {
+    asmjit::CodeHolder code;
+    code.init(runtime.environment());
     
-    asmjit::x86::Assembler* a;
-    std::vector<asmjit::v1_21::Label> labels;
-
-public:
-    CompiledRuntimeFunction(
-        asmjit::x86::Gp stackPtr,
-        asmjit::x86::Gp stackBase,
-        asmjit::x86::Gp framePtr,
-        asmjit::x86::Gp pc,
-        asmjit::x86::Gp memPtr,
-        asmjit::x86::Assembler* a,
-        std::vector<asmjit::v1_21::Label> labels
-    ) : stackPtr(stackPtr), stackBase(stackBase), framePtr(framePtr), pc(pc), memPtr(memPtr), a(a), labels(labels) {}
-};
-
-class CompiledRuntimeFunctionBuilder {
-public:
-    asmjit::x86::Gp stackPtr;
-    asmjit::x86::Gp stackBase;
-    asmjit::x86::Gp framePtr;
-    asmjit::x86::Gp pc;
-    asmjit::x86::Gp memPtr;
+    asmjit::x86::Assembler a(&code);
     
-    asmjit::x86::Assembler* a;
-    std::vector<asmjit::v1_21::Label> labels;
-
-    CompiledRuntimeFunction Build() {
-        return CompiledRuntimeFunction {
-            stackPtr, stackBase, framePtr, pc, memPtr, a, std::move(labels)
-        };
+    // prologue
+    a.push(asmjit::x86::rbp);
+    a.mov(asmjit::x86::rbp, asmjit::x86::rsp);
+    
+    a.push(asmjit::x86::rbx);
+    a.push(asmjit::x86::r12);
+    a.push(asmjit::x86::r13);
+    a.push(asmjit::x86::r14);
+    a.push(asmjit::x86::r15);
+    
+    asmjit::x86::Gp stackBase = asmjit::x86::r13;
+    a.mov(stackBase, asmjit::x86::rdi);
+    
+    asmjit::x86::Gp stackPtr = asmjit::x86::r12;
+    a.mov(stackPtr, stackBase);
+    a.add(stackPtr, function.locals_count * 4);
+    
+    for (const auto& op : function.code) {
+        CompileOperation(a, stackPtr, stackBase, op);
     }
-};
+    
+    // epilogue
+    a.pop(asmjit::x86::r15);
+    a.pop(asmjit::x86::r14);
+    a.pop(asmjit::x86::r13);
+    a.pop(asmjit::x86::r12);
+    a.pop(asmjit::x86::rbx);
+    a.pop(asmjit::x86::rbp);
+    a.ret();
+    
+    void* funcPtr = nullptr;
+    runtime.add(&funcPtr, &code);
+    
+    return CompiledRuntimeFunction(funcPtr, code.code_size());
+}
 
-class X86JitCompiler : public JitCompiler {
-    // TODO
-};
 
 }  // namespace czffvm_jit
