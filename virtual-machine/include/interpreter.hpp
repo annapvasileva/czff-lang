ff3ad306aa885ae8e73c82ce63009af090f776bf
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2026-01-15 15:00:56 +0300

    feat methods

--- virtual-machine/include/interpreter.hpp
+++ virtual-machine/include/interpreter.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "runtime_data_area.hpp"
+#include "jit/jit_x86_64.hpp"
 
 namespace czffvm {
 
@@ -10,8 +11,16 @@ public:
 
     void Execute(RuntimeFunction* entry);
 
+    void SetJitCompiler(std::unique_ptr<czffvm_jit::JitCompiler> jit);
+
+    void JitCompile(RuntimeFunction* function);
+    void ExecuteJitFunction(RuntimeFunction* function, CallFrame& caller_frame, std::vector<Value>& args);
+    bool CanCompile(const RuntimeFunction* function);
+
 private:
     RuntimeDataArea& rda_;
+    std::unique_ptr<czffvm_jit::JitCompiler> jit_compiler_;
+    std::unique_ptr<czffvm_jit::X86JitHeapHelper> heapHelper_;
 };
 
 } // namespace czffvm
